0ee5fa0145ef170fd1b7fe3247ebaea367c356f4
vladimirsouza <vladimirbcs@gmail.com>
2024-03-19 13:22:15 -0700

    Add public gamblr ashm track hubs and code

---
+++ scripts/ashm_hubs.R
@@ -0,0 +1,41 @@
+### build track hubs for public SSMs data from aSHM reagions, pathologies BL, 
+### DLBCL and FL, seq type genome and capture, projection grch37 and hg38
+
+library(GAMBLR.utils)
+library(GAMBLR.data)
+library(dplyr)
+
+# get metadata
+my_meta = get_gambl_metadata(seq_type_filter = c("genome", "capture")) %>% 
+  filter(pathology %in% c("BL", "DLBCL", "FL"))
+
+# create hub for grch37
+build_browser_hub(
+  these_samples_metadata = my_meta,
+  these_seq_types = c("genome", "capture"),
+  projection = "grch37",
+  local_web_host_dir = "~/repos/LLMPP",
+  hub_dir = "hubs/ashm_hubs",
+  as_bigbed = TRUE,
+  splitColumnName = "pathology",
+  hub_name = "gamblr_ashm", 
+  shortLabel = "gamblr ashm", 
+  longLabel = "GAMBLR public aSHM mutations from grch37 projection",
+  email = "rdmorin@sfu.ca"
+)
+
+# create hub for hg38
+build_browser_hub(
+  these_samples_metadata = my_meta,
+  these_seq_types = c("genome", "capture"),
+  projection = "hg38",
+  local_web_host_dir = "~/repos/LLMPP",
+  hub_dir = "hubs/ashm_hubs",
+  as_bigbed = TRUE,
+  splitColumnName = "pathology",
+  hub_name = "gamblr_ashm", 
+  shortLabel = "gamblr ashm", 
+  longLabel = "GAMBLR public aSHM mutations from hg38 projection",
+  email = "rdmorin@sfu.ca"
+)
+
